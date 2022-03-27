8082adab5d0c5aebd3cdb80bb89ab999d52c2691
cordel22 <cordelfenevall@gmail.com>
2022-03-27 16:50:49 +0200

    4:09:36 scrpuje tri noviny

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change'
+  },
+  {
+    name: 'guardian',
+    address: 'https://theguardian.com/environment/climate-crisis'
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change'
+  }
+]
+
 const articles = []
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+    .then(response => {
+      const html = response.data
+      
+      const $ = cheerio.load(html)
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).attr('href')
+
+        articles.push({
+          title,
+          url,
+          source: newspaper.name
+        })
+      })
+    })
+})
 
 app.get('/', (req,res) => {
   res.json('Welcome to my Climate Change News API')
@@ -13,22 +47,24 @@ app.get('/', (req,res) => {
 
 app.get('/news', (req,res) => {
   
-  axios.get('https://theguardian.com/environment/climate-crisis')
-    .then((response) => {
-      const html = response.data
-      /* console.log(html) */
-      const $ = cheerio.load(html)
+  // axios.get('https://theguardian.com/environment/climate-crisis')
+  //   .then((response) => {
+  //     const html = response.data
+  //     /* console.log(html) */
+  //     const $ = cheerio.load(html)
 
-      $('a:contains("climate")', html).each(function () {
-        const title = $(this).text()
-        const url = $(this).attr('href')
-        articles.push({
-          title,
-          url
-        })
-      })
-      res.json(articles)
-    }).catch((err) => console.log(err))
+  //     $('a:contains("climate")', html).each(function () {
+  //       const title = $(this).text()
+  //       const url = $(this).attr('href')
+  //       articles.push({
+  //         title,
+  //         url
+  //       })
+  //     })
+  //     res.json(articles)
+  //   }).catch((err) => console.log(err))
+
+  res.json(articles)
 })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
